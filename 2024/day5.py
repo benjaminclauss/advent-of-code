6de404364af8681158473c5e85d96ef66bcb9b0e
Benjamin Clauss <benjaminclauss@protonmail.com>
2024-12-05 16:16:57 +0100

    Complete Day 5 Part 2

--- 2024/day5.py
+++ 2024/day5.py
@@ -19,8 +19,6 @@ page_ordering_violations = set()
 for rule in page_ordering_rules:
     page_ordering_violations.add((rule[1], rule[0]))
 
-sum_of_middle_page_numbers = 0
-
 def is_correctly_ordered(update):
     page_orders = []
     for x in range(len(update)):
@@ -32,6 +30,8 @@ def is_correctly_ordered(update):
             return False
     return True
 
+sum_of_middle_page_numbers = 0
+
 for update in pages_to_produce_in_each_update:
     if is_correctly_ordered(update):
         middle_page_number = update[len(update) // 2]
@@ -41,3 +41,27 @@ print(sum_of_middle_page_numbers)
 
 # Part 2
 
+def correct(update):
+    page_orders = []
+    for x in range(len(update)):
+        for y in range(x + 1, len(update)):
+            page_orders.append((update[x], update[y]))
+    
+    for ordering in page_orders:
+        if ordering in page_ordering_violations:
+            idx1 = update.index(ordering[0])
+            idx2 = update.index(ordering[1])
+            update[idx1], update[idx2] = update[idx2], update[idx1]
+            return correct(update)
+    return update
+
+sum_of_middle_page_numbers = 0
+
+for update in pages_to_produce_in_each_update:
+    if is_correctly_ordered(update):
+        continue
+    corrected = correct(update)
+    middle_page_number = update[len(update) // 2]
+    sum_of_middle_page_numbers += middle_page_number
+
+print(sum_of_middle_page_numbers)

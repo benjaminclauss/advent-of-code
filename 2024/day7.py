18a216cd018c035b2db9e02f03c314294998c999
Benjamin Clauss <benjaminclauss@protonmail.com>
2024-12-07 14:16:41 +0100

    Complete Day 7 Part 2

--- 2024/day7.py
+++ 2024/day7.py
@@ -23,13 +23,40 @@ for equation in equations:
             multiplication = previous_evaluation * current_number
             next_evaluations.append(multiplication)
         # Remove evaluations exceeding test value.
-        # next_evaluations = [evaluation for evaluation in next_evaluations if evaluation <= test_value]
+        # Any addition or multiplication would exceed test value.
+        next_evaluations = [evaluation for evaluation in next_evaluations if evaluation <= test_value]
         evaluations = next_evaluations
     
     test_value_evaluation = any(number == test_value for number in evaluations)
     if test_value_evaluation:
         total_calibration_result += test_value
-    print(evaluations)
-    print(test_value_evaluation)
+
+print(total_calibration_result)
+
+# Part 2
+
+total_calibration_result = 0
+
+for equation in equations:
+    test_value, numbers = equation
+    evaluations = []
+    evaluations.append(numbers[0])
+    for current_number in numbers[1:]:
+        next_evaluations = []
+        for previous_evaluation in evaluations:
+            addition = previous_evaluation + current_number
+            next_evaluations.append(addition)
+            multiplication = previous_evaluation * current_number
+            next_evaluations.append(multiplication)
+            concatenation = int(str(previous_evaluation) + str(current_number))
+            next_evaluations.append(concatenation)
+        # Remove evaluations exceeding test value.
+        # Any addition or multiplication would exceed test value.
+        next_evaluations = [evaluation for evaluation in next_evaluations if evaluation <= test_value]
+        evaluations = next_evaluations
+    
+    test_value_evaluation = any(number == test_value for number in evaluations)
+    if test_value_evaluation:
+        total_calibration_result += test_value
 
 print(total_calibration_result)
